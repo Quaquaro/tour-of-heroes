96de2df006eaaf99681da9d7c86373a5db88c3e6
Christoph Timm <quaquaro@gmail.com>
2022-05-22 23:17:12 +0200

    :sparkles: Show editable List of heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
-import { Hero } from './../hero';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,13 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-    specialSkill: 'Weather mastery',
-    hitpoints: 100,
-  };
-
+  heroes = HEROES;
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
   constructor() {}
 
   ngOnInit(): void {}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,23 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Dr. Nice', specialSkill: 'Weather mastery', hitpoints: 100 },
+  { id: 13, name: 'Bombasto', specialSkill: 'Weather mastery', hitpoints: 100 },
+  {
+    id: 14,
+    name: 'Celeritas',
+    specialSkill: 'Weather mastery',
+    hitpoints: 100,
+  },
+  { id: 15, name: 'Magneta', specialSkill: 'Weather mastery', hitpoints: 100 },
+  {
+    id: 16,
+    name: 'RubberMan',
+    specialSkill: 'Weather mastery',
+    hitpoints: 100,
+  },
+  { id: 17, name: 'Dynama', specialSkill: 'Weather mastery', hitpoints: 100 },
+  { id: 18, name: 'Dr. IQ', specialSkill: 'Weather mastery', hitpoints: 100 },
+  { id: 19, name: 'Magma', specialSkill: 'Weather mastery', hitpoints: 100 },
+  { id: 20, name: 'Tornado', specialSkill: 'Weather mastery', hitpoints: 100 },
+];
